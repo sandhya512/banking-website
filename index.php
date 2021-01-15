41f77f160853b4b416511323273e2a3a47f4734e
sandhya512 <77498607+sandhya512@users.noreply.github.com>
2021-01-15 20:31:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+      button{
+        background-color: white;
+      }
+      body{
+        background-image: url("img/bank6.jpg");
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+
+      }
+    </style>
+</head>
+<body >
+<div id="header">
+       <br>
+       <h1 style=" font-family:Agency FB; font-size: 70px  ; "> Sparks Foundation Internship Project </h1>
+       <br>
+       <h2 style=" font-family:Agency FB; font-size: 55px  ; "> Credit management </h2>
+       <br>
+       <h3 style=" font-family:Agency FB; font-size: 40px  ; "> presented by k.sandhya kumari </h3>
+        </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr>        
+               
+                <div class="css-button" >
+                  <p class="css-button-text">View Users</p>
+                  <div class="css-button-inner">
+                  <a href="getdetail.php" >
+                  <div class="reset-skew">
+                  <p class="css-button-inner-text">View Users</p>
+                </div></a>
+                </div>
+                </div>
+                </td>
+                </tr>
+                <tr>        
+               <br> <br> <br> <br> <br>
+               <div class="css-button" >
+                 <p class="css-button-text">Credit Transfer</p>
+                 <div class="css-button-inner">
+                 <a href="selectuser.php" >
+                 <div class="reset-skew">
+                 <p class="css-button-inner-text">Credit Transfer</p>
+               </div></a>
+               </div>
+               </div>
+               </td>
+               </tr>
+            </table>
+    </div>
+</body>
+</html>
